99dacea7faf1b8b7e160d8afd0b68a37136d68e0
Yars2021 <yaroslav.sukhovey@yandex.ru>
2025-12-06 20:30:48 +0300

    .dot generation: edge duplication fixed

--- utils/src/main/kotlin/solace/dotgen/DOTGraph.kt
+++ utils/src/main/kotlin/solace/dotgen/DOTGraph.kt
@@ -1,31 +1,46 @@
-package solace.utils.dotgen;
+package solace.dotgen
+
+import java.util.*
+
+
+class DNP (
+    val node: String,
+    val port: String
+) {
+    override fun toString(): String = node + "_" + port
+    override fun equals(other: Any?): Boolean = (other is DNP) && node == other.node && port == other.port
+    override fun hashCode(): Int = Objects.hash(node) + Objects.hash(port)
+}
 
 
 class DOTConnection(
-    val sourceNodeName: String,
-    val sourcePortName: String,
-    val destinationNodeName: String,
-    val destinationPortName: String
+    val source: DNP,
+    val destination: DNP
 ) {
-    fun getString(): String {
-        return if (sourceNodeName == destinationNodeName && sourcePortName == destinationPortName) {
-            "$sourceNodeName -> $destinationNodeName [label=\"$sourcePortName\"];"
+    override fun toString(): String =
+        if (source.node == destination.node && source.port == destination.port) {
+            "${source.node} -> ${destination.node} [label=<table>\"${source.port}\"</table>];"
         } else {
-            "$sourceNodeName -> $destinationNodeName [taillabel=\"$sourcePortName\",headlabel=\"$destinationPortName\"];"
+            "${source.node} -> ${destination.node} [taillabel=\"${source.port}\",headlabel=\"${destination.port}\"];"
         }
-    }
 }
 
 
 class DOTNetwork(val connections: Array<DOTConnection>) {
-    fun getString(): String {
-        return """
-            digraph Network {
-                node [shape=rect,color=gray];
-                edge [color=gray];
-                
-                ${(connections.map { it.getString() }).reduce { acc, cstr -> acc + cstr }}
-            }
-        """.trimIndent()
-    }
+    override fun toString(): String = (
+        arrayOf(
+            "digraph Network {\n",
+            "\tnode [shape=rect,color=gray];\n",
+            "\tedge [color=gray];\n\n"
+        ) + ((connections.groupBy { it.source }).toList().map { (key, value) ->
+            if (value.size == 1) arrayOf("\t${value[0].toString()}\n")
+            else arrayOf(
+                "\n\tsubgraph helper_$key {\n",
+                "\t\tnode [shape=circle,width=0,label=\"\",color=gray];\n",
+                "\t\thelper_$key;\n",
+                "\t}\n\n",
+                "\t${value[0].source.node} -> helper_${key} [arrowhead=none,taillabel=\"${value[0].source.port}\"];\n"
+            ) + (value.map { "\thelper_${key} -> ${it.destination.node} [headlabel=${it.destination.port}];\n" })
+        }).reduce { acc, sublist -> acc + sublist } + "}"
+    ).reduce { acc, cstr -> acc + cstr }
 }
--- utils/src/main/kotlin/solace/dotgen/Runner.kt
+++ utils/src/main/kotlin/solace/dotgen/Runner.kt
@@ -1,11 +1,12 @@
-package solace.utils.dotgen
+package solace.dotgen
 
-import solace.utils.dotgen.*
 
 class App {
     val greeting: String
         get() {
             /*
+                Input:
+
                 .con $counter@out $add@in1
                 .immcon $add@in2 #1
                 .con $add@out $x@in
@@ -14,12 +15,34 @@ class App {
              */
 
             return DOTNetwork(arrayOf<DOTConnection>(
-                DOTConnection("counter", "out", "add", "in1"),
-                DOTConnection("1", "", "add", "in2"),
-                DOTConnection("add", "out", "x", "in"),
-                DOTConnection("x", "out", "numbers", "in"),
-                DOTConnection("x", "out", "counter", "in")
-            )).getString()
+                DOTConnection(DNP("counter", "out"), DNP("add", "in1")),
+                DOTConnection(DNP("1", ""), DNP("add", "in2")),
+                DOTConnection(DNP("add", "out"), DNP("x", "in")),
+                DOTConnection(DNP("x", "out"), DNP("numbers", "in")),
+                DOTConnection(DNP("x", "out"), DNP("counter", "in"))
+            )).toString()
+
+            /*
+                Output:
+
+                digraph Network {
+                        node [shape=rect,color=gray];
+                        edge [color=gray];
+
+                        counter -> add [taillabel="out",headlabel="in1"];
+                        1 -> add [taillabel="",headlabel="in2"];
+                        add -> x [taillabel="out",headlabel="in"];
+
+                        subgraph helper_x_out {
+                                node [shape=circle,width=0,label="",color=gray];
+                                helper_x_out;
+                        }
+
+                        x -> helper_x_out [arrowhead=none,taillabel="out"];
+                        helper_x_out -> numbers [headlabel=in];
+                        helper_x_out -> counter [headlabel=in];
+                }
+             */
         }
 }
 
