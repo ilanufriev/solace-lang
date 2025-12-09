5af3da282e0fc9609221cceba67d8216d90c396e
Ilia Anufriev <anufriewwi@rambler.ru>
2025-12-09 03:52:25 +0300

    Added fifo functionality to Harv

--- vm/harv/src/main/kotlin/solace/vm/internal/harv/Types.kt
+++ vm/harv/src/main/kotlin/solace/vm/internal/harv/Types.kt
@@ -1,5 +1,7 @@
 package solace.vm.internal.harv
 
+import jdk.jshell.spi.ExecutionControlProvider
+
 interface HarvVal
 class HarvInt(var value: Int) : HarvVal {
     companion object staticData {
@@ -12,9 +14,25 @@ class HarvString(var value: String) : HarvVal {
     }
 }
 class HarvIdentifier(var value: String) : HarvVal {}
-class HarvFifo(var value: String) : HarvVal {
+class HarvFifo(var name: String) : HarvVal {
+    class HarvFifoIsEmpty() : Exception("Fifo is empty")
+
+    var queue = mutableListOf<Int>()
+
     companion object staticData {
         const val typeName = "fifo"
+    }
+
+    fun putToFifo(data: Int) {
+        queue.addLast(data)
+    }
+
+    fun pushFromFifo(): Int {
+        if (queue.isEmpty()) {
+            throw HarvFifoIsEmpty()
+        }
+
+        return queue.removeFirst()
     }
 }
 
